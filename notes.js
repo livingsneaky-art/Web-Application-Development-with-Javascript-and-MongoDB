8b2600af68b0087a775099869a77478d5458b17d
livingsneaky-art <ryanllamido@gmail.com>
2023-03-24 18:32:42 +0800

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,28 @@
+//Course overview
+//Course introduction
+
+//Course outline
+/*
+    At the end of the course, you will be able to:
+        - Create native mobile apps using Meteor.js
+        - Implement social media features, such as following
+        - Write and run unit tests on your JavaScript code
+        - Set up your own server environment to run Meteor applications
+        - Implement the publish and subscribe data control model
+
+    Module 1: MongoDB, Meteor, and reactive data
+    Module 2: User accounts, packages and methods
+    Module 3: Publish subscribe model and MongoDB filters
+    Module 4: SimpleSchemas, autoform and code re-organisation
+*/
+
+//Grading and assessment information
+
+//Prerequisite quiz
+
+
+//Introduction
+//Overview
+
+//Starter code
+
