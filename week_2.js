ff1cd1befbe926841841dd07d43facf60c62b2a1
livingsneaky-art <ryanllamido@gmail.com>
2023-04-05 20:06:07 +0800

    Notes for week 2

--- week_2.js
+++ week_2.js
@@ -12,3 +12,18 @@
 
 
 //Customise the accounts UI
+
+
+//Who is editing? DB
+//Who is editing? working with methods
+
+
+//Who is editing? UI
+
+
+//Improving the UI
+
+
+//User accounts, packages and methods summary
+
+//M-instant part 2: working with methods
