f897083aea56df86c050efd5cf931a6fb01d88f0
livingsneaky-art <ryanllamido@gmail.com>
2023-04-07 14:09:25 +0800

    Finals notes for week 4

---
+++ week_4.js
@@ -0,0 +1,45 @@
+//SimpleSchemas, autoform and code re-organisation
+//Overview
+
+//Learning outcomes
+
+//Source code
+
+
+//Reorganising the code
+
+
+//Fixing bugs
+
+
+//Routing
+
+
+//Iron:router
+
+
+//Grid layout
+
+
+//Find and Findone
+
+
+//Comment box
+
+
+//Comment list
+
+
+//SimpleSChemas, autoorm and code re-organisation summary
+
+//Final version of source code
+
+//M-Instant part 4 - multiple chatrooms!
+
+
+//Course Summary
+
+//Summary
+
+
+
